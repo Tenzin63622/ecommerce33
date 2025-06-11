a0bfa34cce56c8814644fde90bf680036000d801
Tenzin <lamagurutenzin@gmail.com>
2025-06-11 16:35:04 +0545

    Your commit message here

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -35,6 +35,7 @@ export const Navbar = () => {
   }, []);
 
   return (
+    <ClerkProvider>
     <nav className="sticky top-0 z-50 bg-white shadow">
       <div className="container mx-auto flex items-center justify-between px-4 py-4">
         <Link href="/" className="hover:text-blue-600">
@@ -110,5 +111,7 @@ export const Navbar = () => {
         </nav>
       )}
     </nav>
+
+    </ClerkProvider>
   );
 };
